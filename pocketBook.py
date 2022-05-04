cf5f52431c69a9db7df11757c9070647df8ad663
twilm <103089041+twilm@users.noreply.github.com>
2022-05-04 07:41:19 +0000

    Prototype

---
+++ pocketBook.py
@@ -0,0 +1,53 @@
+
+item = []
+def num_of_entries(i):
+    """
+    Counts number of entries
+    this is used to count how many times user has entered something compared to how many entries he would like to make 
+    """
+    count = 0
+    i = int(i)
+    if i != 0:
+        count = i
+        return i
+
+def file_creator(f, l):
+    """
+    Looks for filename in directory,
+    if not found will create and write
+    if found will append
+    """
+    import os
+    filename = f
+    answ=os.path.exists(filename)
+    with open(filename, 'a' if answ else "w") as file:
+        if(answ):
+            file.write("\n")
+        file.write(l)
+
+
+##Need something for encryption as well as removing items from text/crossing off or ticking. Whichever is possible. 
+##Also some sort of reminder function that works with time in OS module
+
+c = num_of_entries(input("How many entries?\n#:"))
+c-=1
+print("Please input each task")
+
+while len(item) <= c:
+    iapp = input("$:")
+    item.append(iapp)
+item = '\n'.join(item)
+istr = str(item)
+nl = "\n".join(istr)
+print(istr)
+file_creator("entries.txt", istr)
+
+
+
+#filename = "entries.txt"
+#answ=os.path.exists(filename)
+#with open(filename, 'a' if answ else "w") as f:
+#   if(answ):
+#       f.write("\n")
+#   f.write(istr)
+
